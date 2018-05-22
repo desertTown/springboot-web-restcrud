5697857615e32436e64ba2834a78a0be936d2340
desertTown <13631260372@163.com>
2018-05-22 22:49:18 +0800

    extract template(Topbar and sideBar), write findAll controller

---
+++ src/main/java/com/jluzh/springboot/controler/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.jluzh.springboot.controler;
+
+import com.jluzh.springboot.dao.EmployeeDao;
+import com.jluzh.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @GetMapping("/emps")
+    public String list(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
+        return "emp/list";
+    }
+}
